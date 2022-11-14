6bffbae1e79e91e2574c9763162a3942d3452fdc
Billy Herculano <billy.herculano@agilesolutions.com>
2022-11-13 20:11:34 -0600

    format

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -21,7 +21,7 @@ const routes: Routes = [
     loadChildren: () =>
       import('./modules/business/business-routing.module').then(
         (m) => m.BusinessRoutingModule,
-      )
+      ),
   },
   {
     //everything else
