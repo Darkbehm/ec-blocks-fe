cf8a6119afc5c4f8de5242889ecb188a087276ac
thglhez <tonyghez99@gmail.com>
2022-12-09 00:27:13 -0600

    Pages CRUD on Frontend

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
-  backendURL: 'http://localhost:3000/ecb-api/',
-  // backendURL: 'https://ec-blocks-be-develop.up.railway.app/ecb-api/',
+  backendURL: 'https://ec-blocks-be-develop.up.railway.app/ecb-api/',
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // backendURL: 'http://localhost:3000/ecb-api/',
   backendURL: 'https://ec-blocks-be-production.up.railway.app/ecb-api/',
 };
